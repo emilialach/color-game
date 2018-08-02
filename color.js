e80e167043908833e8e964693223b08fa1b395f8
emilialach <produkty.tash@gmail.com>
2018-08-02 18:28:58 +0200

    new htlm file

--- color.js
+++ color.js
@@ -10,7 +10,7 @@ var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
 easyBtn.addEventListener("click", function () {
-    clhardBtn.classList.remove("selected");
+    hardBtn.classList.remove("selected");
     easyBtn.classList.add("selected");
     numSquares = 3;
     colors = generateRandomColors(numSquares);
